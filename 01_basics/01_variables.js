16cd04bb62a9d007177c06d1a21bdc5b3ba78c50
Bharat-Odedara <bharatodedara882@gmail.com>
2024-05-08 11:00:24 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 12345;
+let accountEmail = "bharat@gmail.com"
+var accoutPassword = "121212"
+accountCity ="vadodara"
+let accountstate
+// accountId = 56789;  value not change
+
+accountEmail = "ram@gmail.com"
+accountPassword = "123123"
+accountCity = "ahemedabad"
+
+console.log(accountEmail);
+
+/* Preferd Not used var
+Because of issue in block scope and  functional scope */
+console.log([accountPassword , accountCity]);
---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,26 @@
+"use strict";// treat all js code as newer version//
+
+console.log( 3 + 3)
+console.log("Bharat")
+
+
+// tc39.es ecma script standerds
+
+let accountnumber = 8653543634252
+let accountname = "Bharat"
+let isloggedin = true
+let temp;
+let state = null
+
+
+// number => 2 to power 53
+//bigint 
+//string => ""
+//boolean => true/ false
+// undefine => value not assign
+//null => standaloan value
+//symbol => unique
+
+
+//object
+console.log(typeof "bharat")
